3ef13694c031f9c63427dc8888879c2c182072ce
Bruno Ferreira Rocha <bruno_ferreira@hotmail.it>
2022-04-19 12:26:36 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using ExceptionLesson;
+using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo demo = new Demo();
+
+
+            try
+            {
+                int result = demo.GrandparentMethod(4);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+        }
+    }
+}
---
+++ ExceptionLesson/Demo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ExceptionLesson
+{
+    public class Demo
+    {
+        public Demo()
+        {
+
+        }
+        public int GrandparentMethod(int position)
+        {
+            int output = 0;
+            // 5000+ 
+            try
+            {
+                Console.WriteLine("Apri connessione al DB");
+                output = ParentMethod(position); // -> Crash
+                Console.WriteLine(" Write user to DB");
+
+            }
+            catch (DivideByZeroException ex)
+            {
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                //Log.write();// Administrator
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Close connection"); // -> connection opened
+
+            }
+            return output;
+        }
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+        public int GetNumber(int position)
+        {
+            // -> Divisione
+            // 
+            int output = 0;
+
+
+            // leggere da un Array
+            int[] numbers = new int[] { 1, 4, 7, 2 };
+            output = numbers[position];
+
+
+
+            return output;
+        }
+
+    }
+}
